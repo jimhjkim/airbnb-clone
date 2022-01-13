2ddad0f17b3cf5b9f189e63c492b96149a3fa31f
Jim Kim <jim.hyunjin.kim@gmail.com>
2022-01-13 01:28:10 -0500

    Define RoomType, Amenity, Facility, HouseRule Models

--- rooms/models.py
+++ rooms/models.py
@@ -18,14 +18,27 @@ class AbstractItem(core_models.TimeStampedModel):
 
 
 class RoomType(AbstractItem):
-    """RoomType Definition"""
+    """RoomType Model Definition"""
 
     pass
 
 
 class Amenity(AbstractItem):
 
-    """AmenityType Definition"""
+    """Amenity Model Definition"""
+
+    pass
+
+
+class Facility(AbstractItem):
+
+    """Facility Model Definition"""
+
+    pass
+
+
+class HouseRule(AbstractItem):
+    """HouseRule Model Definition"""
 
     pass
 
@@ -48,7 +61,10 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ManyToManyField(RoomType, blank=True)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity)
+    facilities = models.ManyToManyField(Facility)
+    house_rules = models.ManyToManyField(HouseRule)
 
     def __str__(self):
         return self.name
